8b147217a9a97c25b72bc2c77bc40e9b49bb41dc
Animesh Kahara <animeshcyberphysco@gmail.com>
2021-06-22 16:33:29 +0530

    Sidebar Completed

---
+++ js/main.js
@@ -0,0 +1,19 @@
+/* ==== EXPANDING MENU ==== */
+const showMenu = (toggleId, navbarId, bodyId) => {
+  const toggle = document.getElementById(toggleId),
+    navbar = document.getElementById(navbarId),
+    bodypadding = document.getElementById(bodyId);
+
+  if (toggle && navbar) {
+    toggle.addEventListener("click", () => {
+      navbar.classList.toggle("expander");
+
+      bodypadding.classList.toggle("body-pd");
+    });
+  }
+};
+
+showMenu("nav-toggle", "navbar", "body-pd");
+/* ==== LINK ACTIVE ==== */
+
+/* ==== COLLAPSE MENU ==== */
