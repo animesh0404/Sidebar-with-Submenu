0a0ca7ca64906a3c7fd7a7b97093515ec74750cb
Animesh Kahara <animeshcyberphysco@gmail.com>
2021-06-22 16:44:38 +0530

    Active Link feture added

--- js/main.js
+++ js/main.js
@@ -15,5 +15,11 @@ const showMenu = (toggleId, navbarId, bodyId) => {
 
 showMenu("nav-toggle", "navbar", "body-pd");
 /* ==== LINK ACTIVE ==== */
+const linkColor = document.querySelectorAll(".nav__link");
+function colorLink() {
+  linkColor.forEach((l) => l.classList.remove("active"));
+  this.classList.add("active");
+}
 
+linkColor.forEach((L) => L.addEventListener("click", colorLink));
 /* ==== COLLAPSE MENU ==== */
